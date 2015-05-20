067958aa1d616dd786c7c4491d3d5028fb028ffd
banud <banudalamanli@gmail.com>
2015-05-20 10:01:49 -0700

    Refactor and add some puts to track process

--- selection.rb
+++ selection.rb
@@ -1,5 +1,4 @@
-## Sort1 is faster
-
+# PSEUDOCODE:
 # for i = 1:n,
 #     k = i
 #     for j = i+1:n, if a[j] < a[k], k = j
@@ -7,39 +6,23 @@
 #     swap a[i,k]
 # end
 
-# Version 1
-def sort1(collection)
+def sort(collection)
   collection.each_with_index do |el, i|
-    k = i
     j = i+1
     while j < collection.length
-      if collection[j] < collection[k]
-        # p "swapping #{collection[j]} with #{collection[k]}"
-        collection[j], collection[k] = collection[k], collection[j]
+      p "i is #{i}"
+      p "j is #{j}"
+      p collection
+      if collection[j] < collection[i]
+        p "swapping j: #{collection[j]} with i: #{collection[i]}"
+        p "**********************************************"
+        collection[j], collection[i] = collection[i], collection[j]
       end
       j += 1
     end
   end
-  collection
+  p collection
 end
-
-# Version 2
-# def sort2(collection)
-#   k = 0
-#   while k < collection.length
-#     j = 1
-#     while j < collection.length
-#       if collection[j] < collection[k]
-#         # p "swapping #{collection[j]} with #{collection[k]}"
-#         collection[j], collection[k] = collection[k], collection[j]
-#       end
-#       j += 1
-#     end
-#     k += 1
-#   end
-#   collection
-# end
-
 
 
 def benchmark
@@ -49,18 +32,15 @@ def benchmark
 end
 
 # SIMPLE
-# c = [54, 82, 4, 5, 4, 3, 1, 12, 3, 3, 124]
+c = [54, 23, 7, 1, 5]
+# c = [82, 54, 1, 12, 7, 23, 124, 5]
 
 # RANDOM - NOT SORTED
 # c = Array.new(10000) { rand(1..100000) }
-c = Array.new(1000) { rand(1..100000) }
+# c = Array.new(1000) { rand(1..100000) }
 
 # NEARLY SORTED
 # c = Array.new(5000)
 # c.map!.with_index {|el, i| i}
 
-benchmark { sort1(c) }
-
-# sort1_time = benchmark { sort1(c) }
-# sort2_time = benchmark { sort2(c) }
-# p (sort1_time - sort2_time)
+benchmark { sort(c) }
