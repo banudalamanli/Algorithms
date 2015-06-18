bdff43a72df3754373340a99fd68bd6d19a21f60
banud <banudalamanli@gmail.com>
2015-06-18 14:10:12 -0700

    implemented preorder traversal method both recursively and iteratively

--- algorithmic_puzzles_nkarumanchi/Chapter_6_Trees/binary_trees.rb
+++ algorithmic_puzzles_nkarumanchi/Chapter_6_Trees/binary_trees.rb
@@ -1,4 +1,5 @@
 class BinaryTreeNode
+
   attr_accessor :data, :left, :right
 
   def initialize(data = nil, left = nil, right = nil)
@@ -10,12 +11,45 @@ class BinaryTreeNode
   def to_s
     "#{@data.to_s}"
   end
+
 end
 
+####################################
+# EXAMPLE TREE
+####################################
 
 
 class BinaryTree
+  attr_accessor :root
+
   def initialize(data)
     @root = BinaryTreeNode.new(data)
   end
+
+  def preorder_traversal_recursive(start_node = @root) # Time complexity O(n), Space complexity O(n)
+    if start_node != nil
+      preorder_traversal_recursive(start_node.left)
+      puts start_node.to_s
+      preorder_traversal_recursive(start_node.right)
+    end
+  end
+
+  def preorder_traversal_iterative(start_node = @root) # Time complexity O(n), Space complexity O(n)
+    result = []
+    return result if start_node == nil
+
+    stack = []
+    stack << start_node
+    until stack.is_empty?
+      temp_node = stack.pop
+      result << temp_node.data
+      if temp_node.right != nil
+        stack << temp_node.right
+      elsif temp_node.left != nil
+        stack << temp_node.left
+      end
+    end
+    result
+  end
+
 end
