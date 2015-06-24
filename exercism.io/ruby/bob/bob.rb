497d4ecb86e31d74bbe127945c6ae53016783950
banud <banudalamanli@gmail.com>
2015-06-24 15:48:11 -0700

    modified solution to reflect single responsibility

--- exercism.io/ruby/bob/bob.rb
+++ exercism.io/ruby/bob/bob.rb
@@ -1,17 +1,35 @@
 class Bob
+  attr_accessor :remark
+
+  def initialize
+    @remark = nil
+  end
 
   def hey(remark)
-    # Regardless of whether the remark has been phrased as a question or not, when all characters are upcase AND some of them are letters, meaning at least some words are being yelled, then:
-    return 'Whoa, chill out!' if (remark.match(/[A-Z]/) != nil) && remark.upcase == remark
-
-    # When there is no "yelling" and the remark is not a question:
-    if remark[-1] != "?"
-      # When true => there are no "non-white" character (letters, digits, etc.) matches, meaning nothing is said.
-      !remark.match(/\S/) ?
-        'Fine. Be that way!' : 'Whatever.'
-    else # When the remark is a question:
+    @remark = remark
+    case
+    when yelling?
+      'Whoa, chill out!'
+    when question?
       'Sure.'
+    when silence?
+      'Fine. Be that way!'
+    else
+      'Whatever.'
     end
+  end
+
+  private
+  def question?
+    @remark[-1] == "?"
+  end
+
+  def yelling?
+    (@remark.match(/[A-Z]/) != nil) && @remark.upcase == @remark
+  end
+
+  def silence?
+    !@remark.match(/\S/)
   end
 
 end
