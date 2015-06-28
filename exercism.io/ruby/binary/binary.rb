d9debf2f497c88d9dbfbee1f223c7b2376275d71
banud <banudalamanli@gmail.com>
2015-06-28 00:41:52 -0700

    removed @decimal inst var

--- exercism.io/ruby/binary/binary.rb
+++ exercism.io/ruby/binary/binary.rb
@@ -1,17 +1,19 @@
 class Binary
+
   def initialize(number)
     @number = number
-    @decimal = 0
   end
 
   def to_decimal
-    # if there are any characters other than 0 or 1, @decimal is returned
-    return @decimal if @number.index(/[^01]/)
+    result = 0
+    # if there are any characters other than 0 or 1, result is returned
+    return result if @number.index(/[^01]/)
 
     @number.length.times do |i|
-      @decimal += 2**i if @number[-(i+1)] == "1"
+      current_digit = @number[-(i+1)]
+      result += 2**i if current_digit == "1"
     end
-    @decimal
+    result
   end
 
 end
