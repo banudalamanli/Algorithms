1f973d0cbd61509c3180ab71bd421644294d4086
banud <banudalamanli@gmail.com>
2015-07-04 19:34:37 -0700

    implemented percolate down and heapsort - not quite working

--- algorithmic_puzzles_nkarumanchi/Chapter_7_Priority_Queues_and_Heapes/priority_queues_and_heaps.rb
+++ algorithmic_puzzles_nkarumanchi/Chapter_7_Priority_Queues_and_Heapes/priority_queues_and_heaps.rb
@@ -4,30 +4,29 @@ class Heap
   attr_accessor :heap
   attr_reader :type, :size
 
-  def initialize(type = 'min_heap')
-    @heap = []
-    @type = type
-    @size = @heap.size
+  def initialize(data = [])
+    @heap = data
+    @size = data.size
   end
 
   def parent(node_index)
   # for a node at i-th location its parent is at (i-1)/2 th location
-    raise ArgumentError if node_index <= 0 || node_index >= @size
+    return -1 if node_index <= 0 || node_index >= @size
     (node_index - 1) / 2
   end
 
   def left_child(node_index)
   # for a node at i-th location its left child is at 2*i+1 th location
-    left_child = 2 * node_index + 1
-    raise "Node does not exist" if left_child >= @size
-    left_child
+    left = 2 * node_index + 1
+    return -1 if left >= @size
+    left
   end
 
   def right_child(node_index)
-  # for a node at i-th location its parent is at 2*i+2 th location
-    right_child = 2 * node_index + 2
-    raise "Node does not exist" if right_child >= @size
-    left_child
+  # for a node at i-th location its right childis at 2*i+2 th location
+    right = 2 * node_index + 2
+    return -1 if right >= @size
+    right
   end
 
   def get_maximum
@@ -44,40 +43,96 @@ class Heap
     @heap[0] = @heap[@size-1]
     @size   -= 1
 
-    percolate_down(0)
+    # percolate_down
+    # self.heapsort
     max
+  end
+
+  def heapsort!
+    ((@size - 2) / 2).downto(0) {|start| percolate_down(start, @size - 1)}
+
+    (@size - 1).downto(1) do |i|
+      @heap[i], @heap[0] = @heap[0], @heap[i]
+      percolate_down(0, i - 1)
+    end
+    @heap
   end
 
   def destroy
     @heap = nil
-    @type = nil
     @size = nil
   end
 
   private
-  # To heapify
   # Time complexity O(log n), Space complexity O(1)
-  def percolate_down(node_index)
+  def percolate_down(node_index = 0, size = @size)
     left  = left_child(node_index)
     right = right_child(node_index)
 
-    if left_child && @heap[left] > @heap[node_index]
+    if (left != -1) && (@heap[left] > @heap[node_index])
       max = left
     else
       max = node_index
     end
 
-    if right_child && @heap[right] > @heap[node_index]
+    if (right != -1) && (@heap[right] > @heap[max])
       max = right
     end
 
     if max != node_index
-      @heap[node_index], @heap[max] = @heap[max], @heap[node_index]
+      temp              = @heap[node_index]
+      @heap[node_index] = @heap[max]
+      @heap[max]        = temp
+      # @heap[node_index], @heap[max] = @heap[max], @heap[node_index]
     end
-
     percolate_down(max)
   end
+
+  # def percolate_down(node_index = 0, size = @size)
+  #   ############ Note to self - WORK ON THIS: #############
+  #   loop do
+  #     child = node_index * 2 + 1
+  #     break if child > @size
+
+  #     if child + 1 <= @size and @heap[child] < @heap[child + 1]
+  #       child += 1
+  #     end
+
+  #     if @heap[node_index] < @heap[child]
+  #       @heap[node_index], @heap[child] = @heap[child], @heap[node_index]
+  #       node_index = child
+  #     else
+  #       break
+  #     end
+  #   end
+  # end
+
 end
+
+
+
+my_heap = Heap.new([0,  12, 13, 14, 21, 01, 02, 11, 22, 23, 24])
+p my_heap
+p "==" * 25
+my_heap.heapsort!
+p "my heap after sorting:"
+p my_heap
+# p "==" * 25
+# my_heap.size.times do |index|
+#   p "*" * 40
+#   p "#{index}-th Element: #{my_heap.heap[index]}"
+#   p "Left-Child : #{my_heap.heap[my_heap.left_child(index)]}"
+#   p "Right-Child: #{my_heap.heap[my_heap.right_child(index)]}"
+# end
+
+# p "==" * 25
+# p "DELETING MAX"
+# p my_heap.delete_max
+# p "==" * 25
+
+
+
+
 
 
 
