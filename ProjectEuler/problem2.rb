db42961073b22d66129f37a721a83ae514bcedd2
Banu <banud@users.noreply.github.com>
2015-09-04 11:17:11 -0700

    Update problem2.rb
    Refactored solution. 
    Scored 100 on HackerRank. Yay!

--- ProjectEuler/problem2.rb
+++ ProjectEuler/problem2.rb
@@ -1,31 +1,29 @@
-######### PROBLEM 1:
+######### PROBLEM 2:
 ### Even Fibonacci numbers ###
-# By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 
-
-########## SOLUTION: O(1) space complexity, less than O(n) time complexity.
-def sum_of_even_fibonacci_nums_below(number)
-  prev_two_fib = [1, 2]
+########## SOLUTION: ############
+def solution(number)
   sum = 2
-  next_fib = 0
-  while true
-    next_fib = prev_two_fib[0] + prev_two_fib[1]
-    break if next_fib >= number
-    prev_two_fib[0], prev_two_fib[1] = prev_two_fib[1], next_fib
-    sum += next_fib if next_fib % 2 == 0
+  return 2 if number <= 2
+
+  first_fib_num  = 1
+  second_fib_num = 2
+  next_fib_num   = 3
+  count = 0
+
+  while next_fib_num <= number
+    if count == 3
+      sum += next_fib_num
+      count = 0
+    end
+
+    next_fib_num   = first_fib_num + second_fib_num
+    first_fib_num  = second_fib_num
+    second_fib_num = next_fib_num
+    count += 1
   end
   p sum
 end
-
-# def fibonacci_numbers_summing_up_below(number)
-#   first, second, sum = 1, 1, 0
-#   while sum < number
-#     sum += (first + second) if first % 2 ==0 and second % 2 ==0
-#     first, second = first + (2*second), (2*first) + (3*second)
-#   end
-#   p sum
-# end
-
 
 ###################################################################
 
