2e54e7ddb03af791a0e7b786c61d77f202ca39a5
Banu Dalamanli <banudalamanli@gmail.com>
2015-09-28 17:45:14 -0700

    changed folder structure to files

--- ctci/ch1_arrays_and_strings.rb
+++ ctci/ch1_arrays_and_strings.rb
@@ -25,3 +25,12 @@ end
 p unique_no_data_structures("hello")  == false
 p unique_no_data_structures("banana") == false
 p unique_no_data_structures("banu")   == true
+
+
+# 3. Design an algorithm and write code to remove the duplicate characters in a string without using any additional buffer. NOTE: One or two additional variables are fine. An extra copy of the array is not.
+
+####### SOLUTION:
+
+def remove_dup_chars(str)
+  str.squueze
+end
