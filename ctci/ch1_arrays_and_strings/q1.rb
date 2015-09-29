cc51534c1d9e03d3e67e032d8f8f420437481309
Banu Dalamanli <banudalamanli@gmail.com>
2015-09-28 17:23:00 -0700

    solved

--- ctci/ch1_arrays_and_strings/q1.rb
+++ ctci/ch1_arrays_and_strings/q1.rb
@@ -1,5 +1,25 @@
 # 1. Implement an algorithm to determine if a string has all unique characters. 
-# 1.1 What if you can not use additional data structures?
 
 ##### SOLUTION: 
 # 1.
+
+def unique?(str)
+  count_hash = Hash.new(0)
+  str.each_char do |char| 
+    count_hash[char] += 1
+    return false if count_hash[char] == 2
+  end
+  true
+end
+
+p unique?("hello") == false
+p unique?("banu")  == true
+
+
+# 1.1 What if you can not use additional data structures?
+def unique_no_data_structures(str)
+  !str.match(/(\w)\1/)
+end
+
+p unique?("hello") == false
+p unique?("banu")  == true
