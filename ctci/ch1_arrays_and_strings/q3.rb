fea9fdbc9130be5bb0bf31ca276fcb78f4821060
Banu Dalamanli <banudalamanli@gmail.com>
2015-09-28 17:41:43 -0700

    solved with built-in method

---
+++ ctci/ch1_arrays_and_strings/q3.rb
@@ -0,0 +1,7 @@
+# Design an algorithm and write code to remove the duplicate characters in a string without using any additional buffer. NOTE: One or two additional variables are fine. An extra copy of the array is not.
+
+####### SOLUTION:
+
+def remove_dup_chars(str)
+  str.squueze
+end
